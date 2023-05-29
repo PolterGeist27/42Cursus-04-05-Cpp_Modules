2da3d14be6b78e25ff4e33919099ca189cfd9d3d
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-05-29 11:52:51 +0100

    fixed an error on ex02

--- Module 00/ex02/Account.cpp
+++ Module 00/ex02/Account.cpp
@@ -26,13 +26,12 @@ Account::Account( int initial_deposit ) : _amount(initial_deposit)
 {
    // this->_amount = initial_deposit;
     this->_accountIndex = this->getNbAccounts();
-    this->_amount = 0;
-	this->_nbDeposits = 0;
-	this->_nbWithdrawals = 0;
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
 
     Account::_nbAccounts++;
     Account::_totalAmount += initial_deposit;
-    
+
     Account::_displayTimestamp();
     std::cout << "index:" << this->_accountIndex << ";";
     std::cout << "amount:" << this->_amount << ";";
@@ -76,13 +75,13 @@ void    Account::makeDeposit( int deposit )
     std::cout << "index:" << this->_accountIndex << ";";
     std::cout << "p_amount:" << this->_amount<< ";";
     std::cout << "deposit:" << deposit<< ";";
-    
+
     this->_amount += deposit;
     std::cout << "amount:" << this->_amount<< ";";
 
     this->_nbDeposits++;
     std::cout << "deposits:" << this->_nbDeposits << std::endl;
-    
+
     _totalAmount += deposit;
     _totalNbDeposits++;
 }
@@ -100,7 +99,7 @@ bool	Account::makeWithdrawal( int withdrawal )
         return false;
     }
     std::cout << "withdrawal:" << withdrawal << ";";
-    
+
     this->_amount -= withdrawal;
     std::cout << "amount:" << this->_amount<< ";";
 
