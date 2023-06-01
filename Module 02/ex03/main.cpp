90ef6b86e5e91f47e4b9c75dc73724e2283ada2b
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-06-01 12:26:59 +0100

    tests on ex03

--- Module 02/ex03/main.cpp
+++ Module 02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 11:43:13 by diogmart          #+#    #+#             */
-/*   Updated: 2023/05/30 14:27:59 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/06/01 12:26:13 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 
 int main(void)
 {
-    Point const a(0, 0);
-    Point const b(5, 5);
-    Point const c(0, 10);
-    Point const d(0, 1);
+    Point const a(0, 1);
+    Point const b(2, 1);
+    Point const c(1, 3);
+    Point const d(1, 2);
     
     if (bsp(a, b, c, d))
         std::cout << "The point is inside the triangle" << std::endl;
