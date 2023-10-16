19465742fc02d41cf24859c628f15e18c5a19daf
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-10-16 15:06:35 +0100

    finished ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,12 +6,29 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 11:15:33 by diogmart          #+#    #+#             */
-/*   Updated: 2023/10/13 11:17:05 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:03:37 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 int main(void) {
-	
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
 }
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,10 +6,28 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 11:15:41 by diogmart          #+#    #+#             */
-/*   Updated: 2023/10/13 11:16:50 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/10/16 15:04:42 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+
+template <typename T>
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T max(const T& a, const T& b) {
+	return (a > b) ? a : b;
+}
+
+template <typename T>
+T min(const T& a, const T& b) {
+	return (a < b) ? a : b;
+}
+
